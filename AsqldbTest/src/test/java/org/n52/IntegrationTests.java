0a3da4ff074a50d1d830a28e158f970fda6b0a06
Simona Badoiu <simonaandreea.badoiu@gmail.com>
2014-06-09 15:44:50 +0300

    First commit

---
+++ AsqldbTest/src/test/java/org/n52/IntegrationTests.java
@@ -0,0 +1,9 @@
+package test.java.org.n52;
+/**
+ * Test rasdaman multidimensional array queries
+ * @author simona
+ *
+ */
+public class IntegrationTests {
+
+}
---
+++ AsqldbTest/src/test/java/org/n52/UnitTests.java
@@ -0,0 +1,10 @@
+package test.java.org.n52;
+/**
+ * Unit test for the asqldb's JDBC driver support
+ * for multidimensional arrays
+ * @author simona
+ *
+ */
+public class UnitTests {
+
+}
